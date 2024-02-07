9eeda70aca2309942997b8ca267ddaeafcb8941e
viraxor <55741238+viraxor@users.noreply.github.com>
2024-02-07 22:28:10 +0100

    fix error

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 
 async def setup(bot):
-    bot.add_cog(Misc(bot))
+    await bot.add_cog(Misc(bot))
 
 class Misc(commands.Cog):
     def __init__(self, bot):
