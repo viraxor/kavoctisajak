b2c545a942d1bb8cf814ef48bd2b60a6ac58597d
viraxor <55741238+viraxor@users.noreply.github.com>
2024-02-07 22:26:27 +0100

    move over anouser's say

---
+++ cogs/misc.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+import discord
+
+async def setup(bot):
+    bot.add_cog(Misc(bot))
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(name="say")
+    async def say(self, ctx, *, arg):
+        await ctx.send(arg)
