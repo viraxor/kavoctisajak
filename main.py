8d82576cbe621750ff1ad68804aa4c49497d999e
viraxor <55741238+viraxor@users.noreply.github.com>
2024-02-07 21:37:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands, tasks
+
+from dotenv import load_dotenv
+from os import getenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="k!", intents=intents)
+
+@bot.command(name="reload", hidden=True)
+async def reload(ctx, name: str):
+    await bot.reload_extension(f"cogs.{name}")
+    await ctx.send("reloaded")
+
+load_dotenv("./.env")
+token = getenv("TOKEN")
+
+@bot.event
+async def on_ready():
+    await bot.load_extension("cogs.greetings")
+    
+bot.run(token)
