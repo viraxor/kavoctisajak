75b23effceb3e6faba6fe12a2652c3506ead7e00
viraxor <55741238+viraxor@users.noreply.github.com>
2024-02-09 12:26:39 +0100

    args faster

--- commandlib.py
+++ commandlib.py
@@ -14,32 +14,30 @@ class Commands():
         "ip": self.ip,
         }
         
-    async def nothing(self, msg):
+    async def nothing(self, msg, args=None):
         pass
         
-    async def hello(self, msg):
+    async def hello(self, msg, args=None):
         await self.bot.reply_to(msg, f"Hello, {msg.from_user.first_name}!")
 
-    async def say(self, msg):
-        args = msg.text[2:].split(" ")
-        output = ' '.join(args[1:])
+    async def say(self, msg, args=None):
+        output = ' '.join(args)
         await self.bot.send_message(msg.chat.id, output)
 
-    async def roll(self, msg):
-        args = msg.text[2:].split(" ")
+    async def roll(self, msg, args=None):
         try:
-            number = random.randint(1, int(args[1]))
+            number = random.randint(1, int(args[0]))
         except:
             number = random.randint(1, 6)
         await self.bot.send_message(msg.chat.id, f"{msg.from_user.first_name} rolls {number}!")
 
-    async def cock(self, msg):
+    async def cock(self, msg, args=None):
         await self.bot.reply_to(msg, "8" + random.randint(0,15)*"=" + "D")
 
-    async def github(self, msg):
+    async def github(self, msg, args=None):
         await self.bot.reply_to(msg, "https://github.com/viraxor/kavoctisajak")
 
-    async def ip(self, msg):
+    async def ip(self, msg, args=None):
         await self.bot.reply_to(msg, f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}")
         
     async def process(self, msg):
@@ -51,6 +49,6 @@ class Commands():
             await self.bot.reply_to(msg, "Invalid command.")
         else:
             try:
-                await function(msg)
+                await function(msg, args[1:])
             except Exception as exc:
                 await self.bot.reply_to(msg, f"{type(exc)}: {exc}")
