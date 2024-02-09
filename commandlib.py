1118d840b6c5cf5a43c7529ae424cda883c164ac
viraxor <55741238+viraxor@users.noreply.github.com>
2024-02-09 14:12:30 +0100

    fix+1

--- commandlib.py
+++ commandlib.py
@@ -69,7 +69,7 @@ class Commands():
 
     async def reverse(self, msg, args=None):
         """Usage: k!reverse <sentence> | Reverses the text."""
-        if args == []:
+        if args != []:
             await self.bot.reply_to(msg, ' '.join(args)[::-1])
         elif msg.reply_to_message:
             await self.bot.reply_to(msg, msg.reply_to_message.text[::-1])
